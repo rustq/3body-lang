be53682b4b8222cdb6474f23657fba192749d7e2
MeloAlright <meloalright@gmail.com>
2023-10-24 22:54:36 +0800

    + command usage (#25)

--- three_body_e2021/src/bin/main.rs
+++ three_body_e2021/src/bin/main.rs
@@ -20,8 +20,42 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() > 1 {
-        if (args[1] == "-v") {
-            println!("0.3.0");
+        match args[1].as_str() {
+            "-V" => {
+                println!(env!("CARGO_PKG_VERSION"));
+            }
+            "-c" => {
+                let input = args[2].to_owned();
+                let mut lexer = Lexer::new(&input);
+                let mut parser = Parser::new(lexer);
+                let program = parser.parse();
+                let errors = parser.get_errors();
+
+                if errors.len() > 0 {
+                    for err in errors {
+                        println!("{:?}", err);
+                    }
+                    return;
+                }
+
+                if let Some(evaluated) = evaluator.eval(&program) {
+                    match evaluated {
+                        object::Object::Null => {},
+                        _ => println!("{}\n", evaluated),
+                    }
+                }
+            }
+            _ => {
+                println!("usage: 3body [option] ... [arg] ...
+
+Options and arguments:
+
+-V     : print the 3body-lang version number and exit 
+-h     : print this help message and exit 
+-c cmd : program passed in as string (terminates option list)
+-      : program in repl (default)
+")
+            }
         }
         return;
     }
